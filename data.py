4c50da8d799d8fd4eee1b4d0f4bf8beb8a678693
= <dimabelenov@gmail.com>
2019-03-31 11:25:04 +0300

    adds some routes and fake data

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti pariatur ullam ducimus possimus tenetur, laudantium alias exercitationem, labore quasi molestiae maxime quia magni illo magnam accusantium quis quae voluptatibus.',
+            'author': 'Dmitriy',
+            'create_date': '01-01-2001'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti pariatur ullam ducimus possimus tenetur, laudantium alias exercitationem, labore quasi molestiae maxime quia magni illo magnam accusantium quis quae voluptatibus.',
+            'author': 'Dmitriy',
+            'create_date': '01-02-2001'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti pariatur ullam ducimus possimus tenetur, laudantium alias exercitationem, labore quasi molestiae maxime quia magni illo magnam accusantium quis quae voluptatibus.',
+            'author': 'Dmitriy',
+            'create_date': '01-03-2001'
+        }
+    ]
+    return articles
